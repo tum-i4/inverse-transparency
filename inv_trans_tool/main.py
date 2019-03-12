6168929e994fbbd546445c17bf93301dd64cd07c
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-03-12 17:29:48 +0100

    Adds basic UI code to main

--- inv_trans_tool/main.py
+++ inv_trans_tool/main.py
@@ -3,14 +3,30 @@
 
 """ Main module of the inverse transparency tool """
 
+import sys
+# pylint: disable-msg=no-name-in-module
+from PyQt5.QtWidgets import QMainWindow, QApplication, QStatusBar
+from PyQt5 import uic
 
-def main():
-	print("Nothing here...")
+qtCreatorFile = "gui/main.ui" # Enter file here.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 
-def func(x):
-	return x * 2
+class MonitorTool(QMainWindow, Ui_MainWindow): # type: ignore
+	def __init__(self):
+		super(MonitorTool, self).__init__()
+		self.setupUi(self)
+		self.sign_in_button.clicked.connect(self.sign_in)
+		self.statusbar.showMessage("Ready")
+
+	def sign_in(self):
+		user_id = self.id_edit.text()
+		user_pwd = self.pwd_edit.text()
 
 
 if __name__ == "__main__":
-	main()
+	app = QApplication(sys.argv)
+	window = MonitorTool()
+	window.show()
+
+sys.exit(app.exec_())
