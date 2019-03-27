a9dcb1b3f0d96c09044e8677a29c98af57aad199
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-03-27 11:02:35 +0100

    mapi: Adds scaffold for monitor-api (mapi)

---
+++ monitor_api/mapi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# encoding=utf-8
+
+""" moduleinfo """
+
+import sys
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		# Exit code for Ctrl-C
+		sys.exit(130)
