952c6ef76951103473559be1ea7d0201d47733a6
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-03-27 18:20:11 +0100

    mapi: confluence.ConfluenceApi - Adds SearchResource with GET (raising)

--- monitor_api/api/confluence.py
+++ monitor_api/api/confluence.py
@@ -9,6 +9,8 @@ import logging
 
 from flask_restful import Resource
 
+import api.path
+
 
 BASE_URL = "http://vmpretschner28.informatik.tu-muenchen.de/"
 API_BASE_PATH = "rest/api/"
@@ -32,8 +34,19 @@ class ConfluenceApi(object):
 		my_path:str = logger_base + ".confluence"
 		self.logger = logging.getLogger(name=my_path)
 		# TODO create all resources with the given logger attached
-		self.resources:List[Tuple[Resource, str]] = []
+		self.resources:List[Tuple[Resource, str]] = [
+			(ConfluenceApi.SearchResource(logger_base=my_path), api.path.join(API_BASE_PATH, "search")),
+		]
 
 
 	def get_resources(self) -> List[Tuple[Resource, str]]:
 		return self.resources
+
+
+	class SearchResource(Resource):
+		def __init__(self, logger_base:str):
+			self.logger = logging.getLogger(name=logger_base + ".search")
+
+		def get(self):
+			""" Search for entities in Confluence using the Confluence Query Language (CQL) """
+			raise NotImplementedError()
