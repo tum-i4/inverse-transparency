c9a291ad7b8e82a9b514f61c377bf7f1dfc5c599
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-03-27 20:15:02 +0100

    mapi: api.test_path - Tests for api.path.join()

---
+++ monitor_api/api/test_path.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# encoding=utf-8
+
+""" Test module for api.path """
+
+from typing import List, Tuple
+
+import api.path
+
+
+def test_join():
+	test_cases:List[Tuple[List[str], str]] = [
+		# Always starts with "/", never ends with "/" for normal inputs
+		([""],                  ""),
+		(["abc"],               "/abc"),
+		(["a/", "b/", "c/"],    "/a/b/c"),
+		(["/a", "/b", "/c"],    "/a/b/c"),
+		(["/abc/"],             "/abc"),
+		(["/a", "b/"],          "/a/b"),
+		(["/a/", "/b/", "/c/"], "/a/b/c"),
+		(["a", "b", "c", "d"],  "/a/b/c/d"),
+		# Last component "/" -> trailing slash
+		(["a", "b", "c", "/"],  "/a/b/c/"),
+		(["a", "", "/"],        "/a/"),
+		(["/", "a", "b", "c"],  "/a/b/c"),
+		(["/", "", "a"],        "/a"),
+		(["/", "", "a", "/"],   "/a/"),
+		# Last component "" -> no trailing slash
+		(["a", ""],             "/a"),
+		# First component starts with "http" -> no starting slash
+		(["http://r.a/w/", "x"],   "http://r.a/w/x"),
+		(["http://xyz", "x", "/"], "http://xyz/x/"),
+		# Ensure robustness
+		(["", "a", "", "/b/", "/c", "/", ""], "/a/b/c/"),
+		(["", "", ""],          ""),
+		(["", "", "a"],         "/a"),
+		(["", "", "/a"],        "/a"),
+		(["", "", "/a/"],       "/a"),
+		(["", "", "/a/", ""],   "/a"),
+	]
+
+	for inputs, expected_result in test_cases:
+		actual_result = api.path.join(*inputs)
+		assert expected_result == actual_result
