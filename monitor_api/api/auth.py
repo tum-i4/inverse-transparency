98afdd0cc4f50e6f2d0f1bcdb1702e65c8e12551
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-03-29 11:56:43 +0100

    mapi: api.auth - Adds BasicAuth class for basic authentication

--- monitor_api/api/auth.py
+++ monitor_api/api/auth.py
@@ -2,6 +2,8 @@
 # encoding=utf-8
 """ Authenticatior classes """
 
+import base64
+
 from requests import Request
 
 
@@ -10,3 +12,13 @@ class Authenticator(object):
 	def authenticate(self, request:Request) -> None:
 		""" Authenticate the given request. """
 		raise NotImplementedError()
+
+
+class BasicAuth(Authenticator):
+	""" Basic authentication. """
+	def __init__(self, user:str, password:str):
+		self.auth_header = "Basic " + base64.b64encode(bytes(user + ":" + password, encoding="utf-8")).decode()
+
+	def authenticate(self, request:Request) -> None:
+		""" Authenticate the given request with basic authentication. """
+		request.headers = {"Authorization" : self.auth_header}
