6e0e6db23d6f614c63490f7cd658384ba6f07f6c
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-04-02 15:37:23 +0200

    mapi: log.handler.MEFileHandler - Only emits log.entry.Entry instances

--- monitor_api/log/handler.py
+++ monitor_api/log/handler.py
@@ -2,8 +2,19 @@
 # encoding=utf-8
 """ MEFileHandler class """
 
-from logging import FileHandler
+from logging import FileHandler, LogRecord
+
+import log.entry
 
 
 class MEFileHandler(FileHandler):
-	pass
+	""" Custom handler for a log.entry.Entry object. """
+
+	def __init__(self, filename, mode="r", encoding=None, delay=False):
+		super().__init__(filename, mode, encoding, delay)
+
+	def emit(self, record:LogRecord):
+		if not isinstance(record, log.entry.Entry):
+			return
+
+		super().emit(record)
