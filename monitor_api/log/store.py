ac85a6e0f68e38b7d24b979ad4f0cadaaabf3e5e
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-04-02 17:17:20 +0200

    mapi: log.store.Storage - Implements

--- monitor_api/log/store.py
+++ monitor_api/log/store.py
@@ -2,12 +2,24 @@
 # encoding=utf-8
 """ Log storage """
 
+import time
+import json
+
 from log.entry import Entry
+import log.format
 
 
 class Storage(object):
 	def __init__(self, filename:str, source:str):
-		raise NotImplementedError()
+		self.filename = filename
+		self.source = source
 
 	def add(self, entry:Entry) -> None:
-		raise NotImplementedError()
+		content = {
+			"time" : time.strftime(log.format.ISO_DATE_FORMAT),
+			"source" : self.source
+		}
+		content.update(entry.to_dict())
+		with open(self.filename, "a") as f:
+			json.dump(content, f)
+			f.write("\n")
