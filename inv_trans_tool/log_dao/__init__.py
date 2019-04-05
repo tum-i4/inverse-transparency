19b54d0dccfbf50a8be2f429a3a9a1ba78541a3a
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-04-05 15:21:34 +0200

    itt: log_dao - Adds load_all() skeleton (raising)

--- inv_trans_tool/log_dao/__init__.py
+++ inv_trans_tool/log_dao/__init__.py
@@ -1,3 +1,15 @@
 #!.venv/bin/python3
 # encoding=utf-8
 """ Log DAO module """
+
+import datetime as dt
+from typing import List, Tuple
+
+
+def load_all(user_id:str, start:dt.date = None) -> List[Tuple[str, str, str, dt.datetime]]:
+	"""
+	Load all data uses regarding the given user.
+	
+	:param start: Specify the earliest date to retrieve.
+	"""
+	raise NotImplementedError()
