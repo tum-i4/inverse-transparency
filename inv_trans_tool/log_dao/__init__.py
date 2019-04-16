2e4e7d702b1717c2e7e871d21405964b0a25ef51
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-04-16 18:03:20 +0200

    mapi: log_dao - Implements load_all() (highly inefficient)

--- inv_trans_tool/log_dao/__init__.py
+++ inv_trans_tool/log_dao/__init__.py
@@ -3,12 +3,16 @@
 """ Log DAO module """
 
 import datetime as dt
+import json
 from typing import List, Tuple
+
+LOG_FILE_PATH = "/var/log/mapi.log"
+ISO_DATETIME_FORMAT:str = "%Y-%m-%dT%H:%M:%S%z"
 
 
 def load_all(user_id:str, start:dt.date = None) -> List[Tuple[str, str, str, dt.datetime]]:
 	"""
-	Load all data uses regarding the given user.
+	Load all data uses regarding the given user in the form: "Responsible", "Tool", "Usage", "Date"
 	
 	:param start: Specify the earliest date to retrieve.
 	"""
@@ -16,8 +20,24 @@ def load_all(user_id:str, start:dt.date = None) -> List[Tuple[str, str, str, dt.
 	if not start:
 		start = dt.date(1, 1, 1)
 
-	last_read_date:dt.date = dt.date.today()
-	while last_read_date >= start:
-		pass
+	with open(LOG_FILE_PATH, "r") as log_file:
+		lines:List[str] = log_file.readlines()
 
-	raise NotImplementedError()
+	result:List[Tuple[str, str, str, dt.datetime]] = []
+
+	# We backtrack from the end (newest entry) until reaching the specified start date
+	while lines:
+		line = lines.pop()
+		data = json.loads(line)
+		entry_datetime:dt.datetime = dt.datetime.strptime(data["time"], ISO_DATETIME_FORMAT)
+
+		if entry_datetime.date() < start:
+			break
+
+		responsible = data["user"]
+		tool = data["api"]
+		usage = data["method"]
+
+		result.insert(0, (responsible, tool, usage, entry_datetime))
+
+	return result
