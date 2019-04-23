3aff0ad1d912e95710e24691a56828fc949e0dc4
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-04-23 16:09:12 +0200

    mapi: api.sec - Adds global HASH_SIZE, updates docstrings

--- monitor_api/api/sec.py
+++ monitor_api/api/sec.py
@@ -5,15 +5,17 @@ import hashlib
 import secrets
 from typing import Tuple
 
+HASH_SIZE:int = 16
 
-def password_hash(password:str, salt:bytes, size:int = 16) -> str:
-	""" Generate salt, hash, return both. """
+
+def password_hash(password:str, salt:bytes, size:int = HASH_SIZE) -> str:
+	""" Generate and return hash. """
 	pw_hash:str = hashlib.blake2b(bytes(password, encoding="utf-8"), salt=salt, digest_size=size).hexdigest()
 	return pw_hash
 
 
-def password_hash_gen(password:str, salt_size:int = 8, size:int = 16) -> Tuple[str, bytes]:
-	""" Generate salt, hash, return both. """
+def password_hash_gen(password:str, salt_size:int = 8, size:int = HASH_SIZE) -> Tuple[str, bytes]:
+	""" Generate and return (hash, salt). """
 	salt = secrets.token_bytes(8)
 	pw_hash:str = password_hash(password=password, salt=salt, size=size)
 	return pw_hash, salt
