92941239d4a75f29faa2c098a1e1b2a04b2f925c
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-05 17:13:44 +0200

    jira/dashboardTest: performance-warnings - JS example code

---
+++ jira_plugins/dashboardTest/src/main/resources/js/dashboardTest.js
@@ -0,0 +1,11 @@
+console.log('Hello world!');
+var a = [1, 2, 3];
+console.log(a);
+
+function init() {
+    var box = document.getElementById("spacerbox");
+    box.style.backgroundColor = "red";
+    // document.querySelector("#spacerbox")
+};
+
+init();
