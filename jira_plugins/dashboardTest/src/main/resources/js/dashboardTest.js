7a4cd405dad3e0d30b6d3e051a92ad7e1dfa401c
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-06 17:42:00 +0200

    jira/dashboardTest: performance-warnings - Implements _create_alert()

--- jira_plugins/dashboardTest/src/main/resources/js/dashboardTest.js
+++ jira_plugins/dashboardTest/src/main/resources/js/dashboardTest.js
@@ -7,5 +7,27 @@ function populate() {
     throw "NOT IMPLEMENTED";
 };
 
+/**
+ * Creates an HTML <div> element styled as an alert with the given level and text.
+ * 
+ * @param {String} warning_level
+ *   Either "warning" or "danger"
+ * @param {String} warning_text
+ *   The text to appear on the warning
+ */
+function _create_alert(warning_level, warning_text) {
+    // Invalid warning_level
+    if (["warning", "danger"].indexOf(warning_level) < 0) {
+        throw "Invalid warning level";
+    }
+
+    var alert = document.createElement("DIV");
+    alert.className = "alert alert-" + warning_level;
+    alert.setAttribute("role", "alert");
+    alert.innerHTML = warning_text;
+
+    return alert;
+};
+
 // This script is run after the document has loaded
 populate();
