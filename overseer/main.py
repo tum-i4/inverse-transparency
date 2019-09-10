db756167b12de1ecc0689d9d78da511c68a8c85f
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-10 19:55:37 +0200

    seer: Adds See resource to API

--- overseer/main.py
+++ overseer/main.py
@@ -7,10 +7,17 @@ import logging
 from flask import Flask
 from flask_restful import Api
 
+from api.base import IApi
+from api.see import SeeApi
+
+
 app = Flask(__name__)
 app_api = Api(app)
 
 if __name__ == "__main__":
+
+	see_api:IApi = SeeApi()
+	see_api.add_resources(app_api=app_api)
 
 	# Flask handles Ctrl-C
 	app.run(debug=True)
