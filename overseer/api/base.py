90dd4a12c37a137d6539c2cb088f0beb47b7e789
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-10 15:29:21 +0200

    seer: api.base - Documentation

--- overseer/api/base.py
+++ overseer/api/base.py
@@ -7,6 +7,8 @@ from flask_restful import Api
 
 
 class IApi(ABC):
+	""" Interface for API classes """
 	@abstractmethod
 	def add_resources(self, app_api:Api):
+		""" Adds own resources to the given app API. """
 		raise NotImplementedError()
