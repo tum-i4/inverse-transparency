7d4e2b8e6dbb207cad043337e8f9ec7c9a78a06a
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-13 19:16:00 +0200

    seer: dao.store() - Rudimentary dbm storage

--- overseer/dao/__init__.py
+++ overseer/dao/__init__.py
@@ -1,7 +1,19 @@
 # encoding=utf-8
 """ DAO module """
 
+import datetime as dt
+import dbm
+import os
+import os.path
+
 
 def store(**args):
     """ Placeholder! """
-    print("Got:\t%s" % args)
+
+    if os.path.basename(os.getcwd()) != "overseer":
+        raise RuntimeError("Unexpected CWD")
+
+    with dbm.open("dao/seen.dbm", "c") as db:
+        key: str = dt.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        db[key] = str(args)
+        print("Stored:\t%s" % args)
