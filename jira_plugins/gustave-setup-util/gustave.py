7fb3450c092fdf8c94bdab798906124c5465cf1c
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-13 15:57:44 +0200

    gustave: Basic argparse and connection test

--- jira_plugins/gustave-setup-util/gustave.py
+++ jira_plugins/gustave-setup-util/gustave.py
@@ -3,16 +3,37 @@
 """ gustave sets up Jira
 Name inspired by: M. Gustave, the concierge in "The Grand Budapest Hotel" """
 
+import argparse
 import sys
 
+import requests
+import requests.exceptions
 
-def main():
-	pass
+
+def main(jira_url: str):
+    if not jira_url.startswith("http"):
+        jira_url = f"http://{jira_url}"
+
+    # 0. Check connectivity
+    try:
+        requests.get(jira_url)
+    except requests.exceptions.ConnectionError:
+        print("Connection error...")
+        sys.exit(1)
+
+    # 1. Create user "inv_api_user"
+    pass
 
 
 if __name__ == "__main__":
-	try:
-		main()
-	except KeyboardInterrupt:
-		# Exit code for Ctrl-C
-		sys.exit(130)
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "jira_url", help="The URL of the Jira instance, e.g. localhost:2929/jira"
+        )
+        args = parser.parse_args()
+        main(jira_url=args.jira_url)
+
+    except KeyboardInterrupt:
+        # Exit code for Ctrl-C
+        sys.exit(130)
