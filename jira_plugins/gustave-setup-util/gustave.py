58b8a981a41872b60cdf8f5815228b1036589690
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-13 16:19:56 +0200

    gustave: Adds API path and builds full URL

--- jira_plugins/gustave-setup-util/gustave.py
+++ jira_plugins/gustave-setup-util/gustave.py
@@ -9,10 +9,17 @@ import sys
 import requests
 import requests.exceptions
 
+JIRA_API_PATH = "rest/api/2/user"
+
 
 def main(jira_url: str, login: str):
     if not jira_url.startswith("http"):
         jira_url = f"http://{jira_url}"
+
+    if not jira_url.endswith("/"):
+        jira_url = f"{jira_url}/"
+
+    full_api_url = f"{jira_url}{JIRA_API_PATH}"
 
     try:
         user, password = login.split(":")
