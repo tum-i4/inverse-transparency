9c327f14e79862493ba5397364b7a4f2fe52e1b3
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-13 15:36:08 +0200

    gustave: Adds (empty) file gustave.py

---
+++ jira_plugins/gustave-setup-util/gustave.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# encoding=utf-8
+""" gustave sets up Jira
+Name inspired by: M. Gustave, the concierge in "The Grand Budapest Hotel" """
+
+import sys
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		# Exit code for Ctrl-C
+		sys.exit(130)
