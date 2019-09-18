234fbbac6fdeb90774679d6a399767fbcbb359d7
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-18 22:09:19 +0200

    mapi: api.auth - Clarifies the meaning of the comments

--- monitor_api/api/auth.py
+++ monitor_api/api/auth.py
@@ -19,19 +19,19 @@ _USERS: Dict[str, Tuple[str, str, str, bytes]] = {
         "Mahna",
         "ef736a966ab9eb44d90e49867fc7dd5c",
         b"A\xa6\xf35\xdd\xe6\x05\xb4",
-    ),  # 1234
+    ),  # Password: 1234
     "valentin": (
         "Valentin",
         "Admin",
         "dd9dd401c1a7fd23506e6462f5718ce6",
         b";\xa3\x9c\xe6\x10\xd7\xad\x0c",
-    ),  # super-secure-password
+    ),  # Password: super-secure-password
     "somebody": (
         "Somebody Once",
         "Told Me",
         "dacb2aab01d6bbc4db5ec7b086ec285a",
         b"\x92\xe1#\xac2(\xca\x82",
-    ),  # pwd
+    ),  # Password: pwd
 }
 
 
