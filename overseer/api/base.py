d5701aa451558785cc456b1b3265baf6c5e6d4a5
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-19 14:30:00 +0200

    seer: api.base - Formats code with black

--- overseer/api/base.py
+++ overseer/api/base.py
@@ -6,8 +6,9 @@ from flask_restful import Api
 
 
 class IApi(ABC):
-	""" Interface for API classes """
-	@abstractmethod
-	def add_resources(self, app_api:Api):
-		""" Adds own resources to the given app API. """
-		raise NotImplementedError()
+    """ Interface for API classes """
+
+    @abstractmethod
+    def add_resources(self, app_api: Api):
+        """ Adds own resources to the given app API. """
+        raise NotImplementedError()
