95404301f2700abccc6a11bcee05fc847956d251
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-19 17:16:33 +0200

    seer: store.block - Adds Block class with basic ctor and _hash() function

--- overseer/store/block.py
+++ overseer/store/block.py
@@ -1,1 +1,37 @@
 """ Block class """
+
+import datetime as dt
+import json
+from hashlib import sha3_256
+from typing import Optional
+
+
+class Block(object):
+    """ A block in the chain """
+
+    def __init__(
+        self,
+        index: int,
+        content,
+        previous_hash: str,
+        timestamp: Optional[dt.datetime] = None,
+    ):
+        """ If timestamp is not given, the current time is assumed. """
+
+        self.index: int = index
+        self.content = content
+        self.previous_hash: str = previous_hash
+        self.timestamp: dt.datetime = timestamp or dt.datetime.now()  # .strftime("%Y-%m-%dT%H:%M:%S")
+
+    def _hash(self) -> str:
+        """ Hash the block contents with SHA3_256 and return the hex digest. """
+        stringified: str = json.dumps(
+            {
+                "index": self.index,
+                "content": self.content,
+                "previous_hash": self.previous_hash,
+                "timestamp": self.timestamp,
+            },
+            sort_keys=True,
+        )
+        return sha3_256(stringified.encode()).hexdigest()
