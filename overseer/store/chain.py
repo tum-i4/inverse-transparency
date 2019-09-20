21a5466a9e7a46fa37a51e41835a06e7207010d7
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-20 11:37:44 +0200

    seer: store.chain - Adds Chain class with ctor and _init_chain_with_genesis_block() function

--- overseer/store/chain.py
+++ overseer/store/chain.py
@@ -1,1 +1,19 @@
 """ Chain class """
+
+from typing import List
+
+from .block import Block
+
+
+class Chain(object):
+    """ A chain of blocks """
+
+    def __init__(self):
+        """ Ctor. """
+        self.chain: List[Block] = []
+        self._init_chain_with_genesis_block()
+
+    def _init_chain_with_genesis_block(self) -> None:
+        """ Create a genesis block and append it to the chain. """
+        genesis: Block = Block(0, None, "0")
+        self.chain.append(genesis)
