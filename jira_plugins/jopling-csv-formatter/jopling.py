02cedd3c4e9b4cc678bf574ed53766da11aa2933
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-10-29 19:59:53 +0100

    jopling: Adds basic argument checking

--- jira_plugins/jopling-csv-formatter/jopling.py
+++ jira_plugins/jopling-csv-formatter/jopling.py
@@ -5,14 +5,43 @@ Name inspired by: Jopling, the assassin in "The Grand Budapest Hotel" """
 import argparse
 import csv
 import datetime as dt
+import locale
+import os.path
+import re
 import sys
-from typing import List
+from typing import Dict, List, Set
 
 
 def main(file_paths: List[str], outfile: str):
-    print(file_paths)
-    print(outfile)
-    sys.exit(0)
+    for fp in file_paths:
+        if not os.path.isfile(fp):
+            print(f'Given path "{fp}" is not a valid file.')
+            sys.exit(1)
+
+    data: List[Dict] = []
+    all_keys: Set[str] = set()
+
+    for file_path in file_paths:
+        keys = read_csv(file_path, data)
+        if len(keys) < 2:
+            raise IOError(
+                f'Seem to have misread CSV "{file_path}"...\nParsed keys: {keys}'
+            )
+        all_keys.update(keys)
+
+    # TODO Fix and update fields
+
+    # TODO Write out to new file
+    # IMPORTANT: Write in REVERSE date order to ensure links can be set as best as possible
+
+
+def read_csv(file_path: str, data: List[Dict]) -> List[str]:
+    """
+    Read the given file and drop it into the given list of dicts. Each row will be saved as one dict.
+
+    Returns: List of keys found
+    """
+    raise NotImplementedError()
 
 
 if __name__ == "__main__":
